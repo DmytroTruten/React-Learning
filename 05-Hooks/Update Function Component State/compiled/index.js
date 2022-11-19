f5fd643142cf8fa3850a58d0288729934bc3d071
DmitriyTruten <dmitriy2503a@gmail.com>
2022-11-19 09:54:35 +0200

    learn useState hook

---
+++ 05-Hooks/Update Function Component State/compiled/index.js
@@ -0,0 +1,4 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import ColorPicker from "./ColorPicker.js";
+ReactDOM.render( /*#__PURE__*/React.createElement(ColorPicker, null), document.getElementById("app"));
---
+++ 05-Hooks/Update Function Component State/src/index.js
@@ -0,0 +1,5 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import ColorPicker from "./ColorPicker.js";
+
+ReactDOM.render(<ColorPicker />, document.getElementById("app"));
