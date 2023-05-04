02b6fd3c982986d466c507b1d3bb3175cc262cf1
DmitriyTruten <dmitriy2503a@gmail.com>
2023-05-04 19:36:55 +0300

    learn using custom hooks

---
+++ 08-Custom Hooks/Custom Hook/Counter.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { useCounter } from './hooks/useCounter.js';
+
+export const Counter = () => {
+  const [currentCount, increment] = useCounter();
+
+  return (  
+    <div>
+      <h2>Count: {currentCount}</h2>
+      <button onClick={increment}>Click Me!</button>
+    </div>
+  );
+};
+ 
