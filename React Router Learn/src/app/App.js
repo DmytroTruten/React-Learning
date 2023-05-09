7549cce02eb0865969b85e3f03bc18d55e2b7778
Dmytro Truten <dmitriy2503a@gmail.com>
2023-05-09 22:25:37 +0300

    add first 6 exercises for react-router learning

--- React Router Learn/src/app/App.js
+++ React Router Learn/src/app/App.js
@@ -1,4 +1,10 @@
 import React from "react";
+import {
+  RouterProvider,
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+} from "react-router-dom";
 import About from "../components/About";
 import SignUp from "../components/SignUp";
 import Articles from "../components/Articles";
@@ -10,21 +16,26 @@ import Profile from "../components/Profile";
 import EditProfileForm from "../components/EditProfileForm";
 import Root from "../components/Root";
 
-import { Route } from 'react-router-dom';
-
 import "./App.css";
 
-const router = 
-  /* Wrap this Root Route to create Router here */
-  <Route path="/" element={ <Root/> }>
-    {/* Add Routes here! */}
-  </Route>
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<Root />}>
+      <Route path="/about" element={<About />} />
+      <Route path="/sign-up" element={<SignUp />} />
+      <Route path="/articles" element={<Articles />} />
+      <Route path="articles/:title" element={<Article />} />
+      <Route path="authors/:name" element={<Author />} />
+      <Route path="/categories" element={<Categories />} />
+      <Route path="/profile" element={<Profile />} />
+    </Route>
+  )
+);
 
 function App() {
   return (
     <>
-      { /* Replace below and add Router Provider*/}
-      <p>REPLACE ME WITH A ROUTER PROVIDER</p>
+      <RouterProvider router={router} />
     </>
   );
 }
