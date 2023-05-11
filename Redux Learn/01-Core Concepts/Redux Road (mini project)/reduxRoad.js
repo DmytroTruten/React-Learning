ebec85e41787dc33105ba0b1d521c6866bb5d081
Dmytro Truten <dmitriy2503a@gmail.com>
2023-05-11 20:14:22 +0300

    finish mini project

--- Redux Learn/01-Core Concepts/Redux Road (mini project)/reduxRoad.js
+++ Redux Learn/01-Core Concepts/Redux Road (mini project)/reduxRoad.js
@@ -1,40 +1,50 @@
 const reducer = (state = initialWagonState, action) => {
   switch (action.type) {
     case "gather":
-      return {
+      return (initialWagonState = {
         ...state,
         supplies: state.supplies + 15,
         days: state.days + 1,
-      };
+      });
 
     case "travel":
-      return {
+      const updatedState = {
         ...state,
         supplies: state.supplies - action.payload * 20,
         distance: state.distance + action.payload * 10,
         days: state.days + action.payload,
       };
+      if (updatedState.supplies <= 0) {
+        return state;
+      } else {
+        return (initialWagonState = {
+          ...state,
+          supplies: state.supplies - action.payload * 20,
+          distance: state.distance + action.payload * 10,
+          days: state.days + action.payload,
+        });
+      }
     case "tippedWagon":
-      return {
+      return (initialWagonState = {
         ...state,
         supplies: state.supplies - 30,
         distance: state.distance,
-        days: state.days++,
-      };
+        days: state.days + 1,
+      });
     default:
       return state;
   }
 };
 
-const initialWagonState = {
+let initialWagonState = {
   supplies: 100,
   distance: 0,
   days: 0,
 };
 
 let wagon = reducer(undefined, {});
-console.log(wagon);
 wagon = reducer(initialWagonState, { type: "travel", payload: 1 });
-console.log(wagon);
-wagon = reducer(initialWagonState, { type: "gather"});
+wagon = reducer(initialWagonState, { type: "gather" });
+wagon = reducer(initialWagonState, { type: "tippedWagon" });
+wagon = reducer(initialWagonState, { type: "travel", payload: 3 });
 console.log(wagon);
