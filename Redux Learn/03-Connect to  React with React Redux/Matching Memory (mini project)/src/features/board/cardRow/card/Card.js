79e73356a7b8743aff2543df2b428276acb7fd4e
Dmytro Truten <dmitriy2503a@gmail.com>
2023-05-14 19:39:39 +0300

    add first half of mini project

---
+++ Redux Learn/03-Connect to  React with React Redux/Matching Memory (mini project)/src/features/board/cardRow/card/Card.js
@@ -0,0 +1,47 @@
+import React from "react";
+import { useSelector } from "react-redux";
+import { selectVisibleIDs } from "../../boardSlice.js";
+
+let cardLogo =
+  "https://static-assets.codecademy.com/Courses/Learn-Redux/matching-game/codecademy_logo.png";
+
+export const Card = ({ id, contents }) => {
+  const visibleIDs = useSelector(selectVisibleIDs);
+
+  // flip card action
+  const flipHandler = (id) => {
+    // Add action dispatch below
+  };
+
+  let cardStyle = "resting";
+  let click = () => flipHandler(id);
+
+  let cardText = (
+    <img src={cardLogo} className="logo-placeholder" alt="Card option" />
+  );
+
+  // 1st if statement
+  // implement card id array membership check
+  if (visibleIDs.includes(id)) {
+    cardText = contents;
+    click = () => {};
+  }
+
+  // 2nd if statement
+  // implement card id array membership check
+  if (false) {
+    cardStyle = "matched";
+  }
+
+  // 3rd if statement
+  // implement number of flipped cards check
+  if (false) {
+    click = () => {};
+  }
+
+  return (
+    <button onClick={click} className={`card ${cardStyle}`}>
+      {cardText}
+    </button>
+  );
+};
