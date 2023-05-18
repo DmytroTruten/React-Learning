0b2e8c95f9969f770728164ecbaf1890de0b3cac
Dmytro Truten <dmitriy2503a@gmail.com>
2023-05-18 23:22:43 +0300

    add first exercise with redux middleware

---
+++ Redux Learn/05-Redux Middleware/Write Your Own Middleware/index.js
@@ -0,0 +1,20 @@
+import { createStore, applyMiddleware } from "redux";
+
+const messageReducer = (state = "", action) => {
+  if (action.type === "NEW_MESSAGE") {
+    return action.payload;
+  } else {
+    return state;
+  }
+};
+
+const logger = (storeAPI) => (next) => (action) => {
+  console.log(storeAPI.getState());
+
+  const nextState = next(action);
+  console.log(nextState);
+  return nextState;
+};
+
+const store = createStore(messageReducer, "", applyMiddleware(logger));
+store.dispatch({type: "NEW_MESSAGE", payload: "I WROTE A MIDDLEWARE"})
